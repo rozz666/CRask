85816e3927d8c44a8b96f9192f8b378a90b4f85e
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2012-12-03 22:38:04 +0100

    CodeGenerator generateClassRegistrations should register classes and define its variables

--- lib/crask/code_generator.rb
+++ lib/crask/code_generator.rb
@@ -1,7 +1,19 @@
 module CRask
+  module Ast
+    class Ast::ClassDef
+      def registrationCode
+        "CRASK_CLASS class_#{@name} = crask_registerClass(\"#{@name}\")\n"
+      end
+    end
+  end
+
   class CodeGenerator
     def generateHeaders ast
       "#include <crask.h>\n"
+    end
+
+    def generateClassRegistrations ast
+      ast.stmts.map { |s| s.registrationCode }.join
     end
   end
 end
--- spec/crask/code_generator_spec.rb
+++ spec/crask/code_generator_spec.rb
@@ -4,11 +4,19 @@ module CRask
   describe CodeGenerator do
     before(:each) do
       @cg = CodeGenerator.new
+      @ast = Ast::Ast.new
     end
     context "generateHeaders" do
       it "should include crask.h" do
-        ast = Ast::Ast.new
-        @cg.generateHeaders(ast).should eql("#include <crask.h>\n");
+        @cg.generateHeaders(@ast).should eql("#include <crask.h>\n");
+      end
+    end
+    context "generateClassRegistrations" do
+      it "should register classes and define its variables" do
+        @ast.stmts = [ Ast::ClassDef.new("A"), Ast::ClassDef.new("B") ]
+        @cg.generateClassRegistrations(@ast).should eql(
+        "CRASK_CLASS class_A = crask_registerClass(\"A\")\n" +
+        "CRASK_CLASS class_B = crask_registerClass(\"B\")\n")
       end
     end
   end
