57c972bce8f953fffbb906a12d7cb59e0bb228f9
Rafal Przywarski <rp@rp-Satellite-A200.(none)>
2013-01-28 21:39:33 +0100

    MethodCodeGenerator should generate empty constructors

--- lib/crask/method_code_generator.rb
+++ lib/crask/method_code_generator.rb
@@ -1,11 +1,24 @@
 module CRask
+  module Ast
+    class MethodDef
+      def get_method_code class_name, name_gen
+        method_name = name_gen.get_method_name class_name, name
+        "CRASK_OBJECT #{method_name}(CRASK_OBJECT self, ...) {\n    return CRASK_NIL;\n}\n"
+      end
+    end
+    class CtorDef
+      def get_method_code class_name, name_gen
+        ctor_name = name_gen.get_ctor_name class_name, name
+        "void #{ctor_name}(CRASK_OBJECT self, ...) {\n}"
+      end
+    end
+  end
   class MethodCodeGenerator
     def initialize name_gen
       @name_gen = name_gen
     end
     def generate class_name, method_def
-      method_name = @name_gen.get_method_name class_name, method_def.name
-      "CRASK_OBJECT #{method_name}(CRASK_OBJECT self, ...) {\n    return CRASK_NIL;\n}\n"
+      method_def.get_method_code class_name, @name_gen
     end
   end
 end
--- spec/crask/method_code_generator_spec.rb
+++ spec/crask/method_code_generator_spec.rb
@@ -11,5 +11,9 @@ module CRask
       @gen.generate("A", Ast::MethodDef.new("m")).should eql(
       "CRASK_OBJECT methodName(CRASK_OBJECT self, ...) {\n    return CRASK_NIL;\n}\n")
     end
+    it "should generate empty constructors" do
+      @name_gen.should_receive(:get_ctor_name).with("A", "m").and_return("ctorName")
+      @gen.generate("A", Ast::CtorDef.new("m")).should eql("void ctorName(CRASK_OBJECT self, ...) {\n}")
+    end
   end
 end
