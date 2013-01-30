6067d5409a30eeaad3b9649b734e72c32f66231c
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-01-30 22:13:49 +0100

    DefaultMethodsGenerator should add a destructor to a class if it's not already defined

--- lib/crask/default_methods_generator.rb
+++ lib/crask/default_methods_generator.rb
@@ -1,6 +1,9 @@
 module CRask
   class DefaultMethodsGenerator
     def update_ast ast
+      ast.stmts.each do |s|
+        s.defs << Ast::DtorDef.new unless s.defs.index{ |d| d.kind_of? Ast::DtorDef }
+      end
     end
   end
 end
--- spec/crask/default_methods_generator_spec.rb
+++ spec/crask/default_methods_generator_spec.rb
@@ -8,9 +8,20 @@ module CRask
     end
     it "should not add a destructor to a class if it's already defined" do
       ast = Ast::Ast.with_two_classes_with_dtors
+      ast.stmts[0].defs.insert 0, Ast::MethodDef.new("X")
+      
       @gen.update_ast ast
-      ast.stmts[0].defs.should have(1).item
+      ast.stmts[0].defs.should have(2).item
       ast.stmts[1].defs.should have(1).item
+    end
+    it "should add a destructor to a class if it's not already defined" do
+      ast = Ast::Ast.with_two_classes_with_methods("A", "b", "C", "d")
+      
+      @gen.update_ast ast
+      ast.stmts[0].defs.should have(2).items
+      ast.stmts[0].defs[1].should be_a_kind_of(Ast::DtorDef)
+      ast.stmts[1].defs.should have(2).items
+      ast.stmts[1].defs[1].should be_a_kind_of(Ast::DtorDef)
     end
   end
 end 
