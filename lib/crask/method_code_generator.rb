79b601f23dc62299e89d9d9c38215c08751c2c70
Rafal Przywarski <rafalprzywarski@przywars-Latitude-E5420.(none)>
2013-02-09 11:53:33 +0100

    MethodCodeGenerator should generate a constructor with args from varargs

--- lib/crask/method_code_generator.rb
+++ lib/crask/method_code_generator.rb
@@ -11,9 +11,19 @@ module CRask
         ctor_name = name_gen.get_ctor_name class_name, name
         decorated_class_name = name_gen.get_class_name class_name
         "CRASK_OBJECT #{ctor_name}(CRASK_OBJECT classSelf, ...) {\n"+
+        get_args_decl(name_gen) +
         "    CRASK_OBJECT self = crask_createInstance(#{decorated_class_name});\n" +
         "    return self;\n" +
         "}"
+      end
+      def get_args_decl name_gen
+        return "" if args.empty?
+        decorated_args = args.map { |a| name_gen.get_local_name a }
+        "    CRASK_OBJECT #{decorated_args.join(", ")};\n" +
+        "    va_list rask_args;\n" +
+        "    va_start(rask_args, classSelf);\n" +
+        decorated_args.map { |a| "    #{a} = va_arg(rask_args, CRASK_OBJECT);\n" }.join +
+        "    va_end(rask_args);\n"
       end
     end
     class DtorDef
--- spec/crask/method_code_generator_spec.rb
+++ spec/crask/method_code_generator_spec.rb
@@ -20,6 +20,24 @@ module CRask
         "    return self;\n" +
         "}")
     end
+    it "should generate a constructor with args from varargs" do
+      @name_gen.stub(:get_ctor_name).and_return("ctorName")
+      @name_gen.stub(:get_class_name).and_return("className")
+      @name_gen.should_receive(:get_local_name).with("arg1").and_return("local1")
+      @name_gen.should_receive(:get_local_name).with("arg2").and_return("local2")
+      @gen.generate("A", Ast::CtorDef.new("m", [ "arg1", "arg2" ])).should eql(
+        "CRASK_OBJECT ctorName(CRASK_OBJECT classSelf, ...) {\n" +
+        "    CRASK_OBJECT local1, local2;\n" +
+        "    va_list rask_args;\n" +
+        "    va_start(rask_args, classSelf);\n" +
+        "    local1 = va_arg(rask_args, CRASK_OBJECT);\n" +
+        "    local2 = va_arg(rask_args, CRASK_OBJECT);\n" +
+        "    va_end(rask_args);\n" +
+        "    CRASK_OBJECT self = crask_createInstance(className);\n" +
+        "    return self;\n" +
+        "}")
+                   
+    end
     it "should generate an empty destructor" do
       @name_gen.should_receive(:get_dtor_name).with("A").and_return("dtorName")
       @gen.generate("A", Ast::DtorDef.new).should eql(
