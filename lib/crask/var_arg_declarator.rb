f6ec9e86ce8ad21a87d3903c496493e07f8e3918
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-02-11 12:02:59 +0100

    VarArgDeclarator should declare method args using varargs

---
+++ lib/crask/var_arg_declarator.rb
@@ -0,0 +1,15 @@
+module CRask
+  class VarArgDeclarator
+    def initialize name_gen
+      @name_gen = name_gen
+    end
+    def generate args
+      names = args.map { |a| @name_gen.get_local_name a }
+      "    CRASK_OBJECT #{names.join(", ")};\n" +
+      "    va_list rask_args;\n" +
+      "    va_start(rask_args, classSelf);\n" +
+      names.map { |n| "    #{n} = va_arg(rask_args, CRASK_OBJECT);\n" }.join +
+      "    va_end(rask_args);\n"
+    end
+  end
+end
