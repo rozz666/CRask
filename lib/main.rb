35dab7d7ff60982931e4c2d4b44d4aa2cd682c79
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-02-11 12:54:42 +0100

    refactored main

--- lib/main.rb
+++ lib/main.rb
@@ -2,10 +2,11 @@ require 'crask/parser'
 require 'crask/code_generator_factory'
 require 'crask/code_generator_generate'
 
-source = File.open(ARGV[1]) { |f| f.read }
 parser = CRask::Parser.new
-ast = parser.parse(source)
 default_methods_generator = CRask::DefaultMethodsGenerator.new 
-default_methods_generator.update_ast ast
 code_generator = CRask::CodeGeneratorFactory.new.createCodeGenerator
+
+source = File.open(ARGV[1]) { |f| f.read }
+ast = parser.parse(source)
+default_methods_generator.update_ast ast
 File.open(ARGV[2], "w") { |output| output.write code_generator.generate(ast) }
