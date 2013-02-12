3b20446403cc54913ba2dfaf6f0573c0dafa2689
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-02-12 21:58:32 +0100

    VarArgDeclarator should declare method args using varargs with given self arg

--- lib/crask/var_arg_declarator.rb
+++ lib/crask/var_arg_declarator.rb
@@ -12,5 +12,14 @@ module CRask
       names.map { |n| "    #{n} = va_arg(rask_args, CRASK_OBJECT);\n" }.join +
       "    va_end(rask_args);\n"
     end
+    def generate_from_self_arg self_arg, args
+      return "" if args.empty?
+      names = args.map { |a| @name_gen.get_local_name a }
+      "    CRASK_OBJECT #{names.join(", ")};\n" +
+      "    va_list rask_args;\n" +
+      "    va_start(rask_args, #{self_arg});\n" +
+      names.map { |n| "    #{n} = va_arg(rask_args, CRASK_OBJECT);\n" }.join +
+      "    va_end(rask_args);\n"
+    end
   end
 end
--- spec/crask/var_arg_declarator_spec.rb
+++ spec/crask/var_arg_declarator_spec.rb
@@ -16,8 +16,20 @@ module CRask
         "    local2 = va_arg(rask_args, CRASK_OBJECT);\n" +
         "    va_end(rask_args);\n")
     end
+    it "should declare method args using varargs with given self arg" do
+      @name_gen.should_receive(:get_local_name).with("arg1").and_return("local1")
+      @name_gen.should_receive(:get_local_name).with("arg2").and_return("local2")
+      @arg_decl.generate_from_self_arg("selfArg", [ "arg1", "arg2" ]).should eql(
+        "    CRASK_OBJECT local1, local2;\n" +
+        "    va_list rask_args;\n" +
+        "    va_start(rask_args, selfArg);\n" +
+        "    local1 = va_arg(rask_args, CRASK_OBJECT);\n" +
+        "    local2 = va_arg(rask_args, CRASK_OBJECT);\n" +
+        "    va_end(rask_args);\n")
+    end
     it "should generate nothing for no arguments" do
       @arg_decl.generate([]).should eql("")
+      @arg_decl.generate_from_self_arg("selfArg", []).should eql("")
     end
   end
 end
