a3af0451d024fdf0e2af03fa639df9b4fe7ae976
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-02-12 22:11:11 +0100

    removed unused method from VarArgDeclarator

--- lib/crask/var_arg_declarator.rb
+++ lib/crask/var_arg_declarator.rb
@@ -3,15 +3,6 @@ module CRask
     def initialize name_gen
       @name_gen = name_gen
     end
-    def generate args
-      return "" if args.empty?
-      names = args.map { |a| @name_gen.get_local_name a }
-      "    CRASK_OBJECT #{names.join(", ")};\n" +
-      "    va_list rask_args;\n" +
-      "    va_start(rask_args, classSelf);\n" +
-      names.map { |n| "    #{n} = va_arg(rask_args, CRASK_OBJECT);\n" }.join +
-      "    va_end(rask_args);\n"
-    end
     def generate_from_self_arg self_arg, args
       return "" if args.empty?
       names = args.map { |a| @name_gen.get_local_name a }
--- spec/crask/var_arg_declarator_spec.rb
+++ spec/crask/var_arg_declarator_spec.rb
@@ -5,17 +5,6 @@ module CRask
       @name_gen = double("name generator")
       @arg_decl = VarArgDeclarator.new @name_gen
     end
-    it "should declare method args using varargs" do
-      @name_gen.should_receive(:get_local_name).with("arg1").and_return("local1")
-      @name_gen.should_receive(:get_local_name).with("arg2").and_return("local2")
-      @arg_decl.generate([ "arg1", "arg2" ]).should eql(
-        "    CRASK_OBJECT local1, local2;\n" +
-        "    va_list rask_args;\n" +
-        "    va_start(rask_args, classSelf);\n" +
-        "    local1 = va_arg(rask_args, CRASK_OBJECT);\n" +
-        "    local2 = va_arg(rask_args, CRASK_OBJECT);\n" +
-        "    va_end(rask_args);\n")
-    end
     it "should declare method args using varargs with given self arg" do
       @name_gen.should_receive(:get_local_name).with("arg1").and_return("local1")
       @name_gen.should_receive(:get_local_name).with("arg2").and_return("local2")
@@ -28,7 +17,6 @@ module CRask
         "    va_end(rask_args);\n")
     end
     it "should generate nothing for no arguments" do
-      @arg_decl.generate([]).should eql("")
       @arg_decl.generate_from_self_arg("selfArg", []).should eql("")
     end
   end
