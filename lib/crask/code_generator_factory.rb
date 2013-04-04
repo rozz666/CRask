c19d1f9a467fd64a462ed9ab76f128e0e88f1a58
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-04 22:26:34 +0200

    used StatementCodeGenerator instead of stub

--- lib/crask/code_generator_factory.rb
+++ lib/crask/code_generator_factory.rb
@@ -5,22 +5,17 @@ require 'crask/default_methods_generator'
 require 'crask/var_arg_declarator'
 require 'crask/argument_ordering_policy'
 require 'crask/method_name_generator'
+require 'crask/statement_code_generator'
 
 module CRask
-  
-  class StatementGeneratorStub
-    def generate_statements(*args)
-      ""
-    end
-  end
-  
   class CodeGeneratorFactory
     def createCodeGenerator
       arg_ordering_policy = ArgumentOrderingPolicy.new
       symbol_name_gen = CRask::SymbolNameGenerator.new arg_ordering_policy
       method_name_generator = CRask::MethodNameGenerator.new arg_ordering_policy
       arg_decl = CRask::VarArgDeclarator.new symbol_name_gen
-      method_code_gen = CRask::MethodCodeGenerator.new symbol_name_gen, arg_decl, StatementGeneratorStub.new
+      stmt_gen = CRask::StatementCodeGenerator.new symbol_name_gen
+      method_code_gen = CRask::MethodCodeGenerator.new symbol_name_gen, arg_decl, stmt_gen
       class_gen = CRask::ClassGenerator.new symbol_name_gen, method_name_generator, method_code_gen
       CRask::CodeGenerator.new(symbol_name_gen, method_code_gen, class_gen)
     end
