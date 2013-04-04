d8d39e56bc724624c6749129eac2aae96acb47e3
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-04 22:23:35 +0200

    StatementCodeGenerator should generate nothing when passed no statements

--- lib/crask/statement_code_generator.rb
+++ lib/crask/statement_code_generator.rb
@@ -4,6 +4,7 @@ module CRask
       @name_gen = name_gen
     end
     def generate_statements stmts
+      return "" if stmts.empty?
       "#{@name_gen.get_local_name(stmts[0].left)} = #{@name_gen.get_nil_name}"
     end
   end
--- spec/crask/statements_code_generator_spec.rb
+++ spec/crask/statements_code_generator_spec.rb
@@ -9,7 +9,10 @@ module CRask
     it "should generate nil assignment" do
       @name_gen.should_receive(:get_local_name).with("a").and_return("LOCAL")
       @name_gen.should_receive(:get_nil_name).and_return("NIL")
-      @gen.generate_statements([ Ast::AssignmentDef.new("a","nil") ]).should eql("LOCAL = NIL")
+      @gen.generate_statements([ Ast::AssignmentDef.new("a", "nil") ]).should eql("LOCAL = NIL")
+    end
+    it "should generate nothing when passed no statements" do
+      @gen.generate_statements([]).should eql("")
     end
   end
 end
