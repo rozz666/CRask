2300b1f9d80cbb37b9986b8347e03cec6186ced3
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-05 19:45:42 +0200

    LocalVariableDeclarator uses a name generator

--- lib/crask/local_variable_declarator.rb
+++ lib/crask/local_variable_declarator.rb
@@ -1,8 +1,11 @@
 module CRask
   class LocalVariableDeclarator
+    def initialize name_gen
+      @name_gen = name_gen
+    end
     def generate_variables vars
       return "" if vars.empty?
-      "    CRASK_OBJECT #{vars.join(", ")};\n"
+      "    CRASK_OBJECT #{vars.map{ |v| @name_gen.get_local_name(v) }.join(", ")};\n"
     end
   end
 end
--- spec/crask/local_variable_declarator_spec.rb
+++ spec/crask/local_variable_declarator_spec.rb
@@ -3,16 +3,20 @@ require 'crask/local_variable_declarator'
 module CRask
   describe :LocalVariableDeclarator do
     before(:each) do
-      @decl = LocalVariableDeclarator.new
+      @name_gen = double("name generator")
+      @decl = LocalVariableDeclarator.new @name_gen
     end
     it "should return nothing for no variables" do
       @decl.generate_variables([]).should eql("")
     end
     it "should declare one variable" do
-      @decl.generate_variables([ "var" ]).should eql("    CRASK_OBJECT var;\n")
+      @name_gen.should_receive(:get_local_name).with("var").and_return("VAR")
+      @decl.generate_variables([ "var" ]).should eql("    CRASK_OBJECT VAR;\n")
     end
     it "should use one declaration for all variables" do
-      @decl.generate_variables([ "x", "y", "A" ]).should eql("    CRASK_OBJECT x, y, A;\n")
+      @name_gen.should_receive(:get_local_name).with("x").and_return("X")
+      @name_gen.should_receive(:get_local_name).with("y").and_return("Y")
+      @decl.generate_variables([ "x", "y" ]).should eql("    CRASK_OBJECT X, Y;\n")
     end
   end
 end
