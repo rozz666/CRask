50220ff71a6b93712013afdfddda5a7d4536d0fa
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-05 22:23:57 +0200

    CModuleBuilder should build empty include list by default

--- lib/crask/c_module_builder.rb
+++ lib/crask/c_module_builder.rb
@@ -2,8 +2,10 @@ require 'crask/cast/Module'
 
 module CRask
   class CModuleBuilder
+    def initialize
+      @includes = []
+    end
     def add_include name
-      @includes ||= []
       @includes << name
     end
     def build
--- spec/crask/c_module_builder_spec.rb
+++ spec/crask/c_module_builder_spec.rb
@@ -8,6 +8,9 @@ module CRask
     it "should build CAst::Module" do
       @builder.build.should be_a_kind_of(CAst::Module)
     end
+    it "should build empty include list by default" do
+      @builder.build.includes.should eql([])
+    end
     it "should add includes" do
       @builder.add_include "abc"
       @builder.add_include "def"
