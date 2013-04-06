366badedc3cd99e5fbfbb5c59ebc21a68d8698fe
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-06 10:15:35 +0200

    CIncludePrinter should print C includes and append additional new line

--- lib/crask/c_include_printer.rb
+++ lib/crask/c_include_printer.rb
@@ -1,7 +1,8 @@
 module CRask
   class CIncludePrinter
     def print includes
-      ""
+      return "" if includes.empty?
+      includes.map { |i| "#include <#{i}>\n" }.join + "\n"
     end
   end
 end
--- spec/crask/c_include_printer_spec.rb
+++ spec/crask/c_include_printer_spec.rb
@@ -8,5 +8,8 @@ module CRask
     it "should print nothing for empty list" do
       @printer.print([]).should eql("")
     end
+    it "should print C includes and append additional new line" do
+      @printer.print([ "first", "second" ]).should eql("#include <first>\n#include <second>\n\n")
+    end
   end
 end
