6fb7a323dc7e0df5ee7b53db315e604fe7dbcdde
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-06 11:32:52 +0200

    refactored CFunctionPrinter spec

--- spec/crask/c_function_printer_spec.rb
+++ spec/crask/c_function_printer_spec.rb
@@ -4,14 +4,15 @@ module CRask
   describe :CFunctionPrinter do
     before(:each) do
       @local_variable_printer = double("local variable printer")
+      @local_variable_printer.stub(:print).and_return("")
       @statement_printer = double("statement printer")
+      @statement_printer.stub(:print).and_return("")
       @arg_printer = double("arg printer")
+      @arg_printer.stub(:print).and_return("")
       @printer = CFunctionPrinter.new @arg_printer, @local_variable_printer, @statement_printer
     end
     it "should print type, name and arguments" do
       function = CAst::Function.new "type", "name", :args, nil, nil
-      @local_variable_printer.stub(:print).and_return("")
-      @statement_printer.stub(:print).and_return("")
       @arg_printer.should_receive(:print).with(:args).and_return("ARGS")
             
       @printer.print(function).should eql("type name(ARGS) {\n}\n")
@@ -20,15 +21,12 @@ module CRask
       function = CAst::Function.new "", "", nil, :local_vars, :statements
       @local_variable_printer.should_receive(:print).with(:local_vars).and_return("LOCAL_VARS\n")
       @statement_printer.should_receive(:print).with(:statements).and_return("STATEMENTS\n")
-      @arg_printer.stub(:print).and_return("")
             
       @printer.print(function).should include("{\n    LOCAL_VARS\n    STATEMENTS\n}")
     end
     it "should ident local variables" do
       function = CAst::Function.new "", "", nil, :local_vars, nil
       @local_variable_printer.should_receive(:print).with(:local_vars).and_return("LINE1\nLINE2\n")
-      @statement_printer.stub(:print).and_return("")
-      @arg_printer.stub(:print).and_return("")
             
       @printer.print(function).should include("{\n    LINE1\n    LINE2\n}")
     end
