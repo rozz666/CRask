7cdd83867229df06a573b67b77f4078af7af9556
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-06 10:28:00 +0200

    CGlobalVariablePrinter should print C global variable declarations and append additional new line

--- lib/crask/c_global_variable_printer.rb
+++ lib/crask/c_global_variable_printer.rb
@@ -1,7 +1,8 @@
 module CRask
   class CGlobalVariablePrinter
     def print vars
-      ""
+      return "" if vars.empty?
+      vars.map { |v| "#{v.type} #{v.name};\n" }.join + "\n"
     end
   end
 end
--- spec/crask/c_global_variable_printer_spec.rb
+++ spec/crask/c_global_variable_printer_spec.rb
@@ -8,5 +8,9 @@ module CRask
     it "should print nothing for empty list" do
       @printer.print([]).should eql("")
     end
+    it "should print C global variable declarations and append additional new line" do
+      vars = [ CAst::GlobalVariable.new("type1", "name1"), CAst::GlobalVariable.new("type2", "name2") ]
+      @printer.print(vars).should eql("type1 name1;\ntype2 name2;\n\n")
+    end
   end
 end
