00f493467c02134c07242b823f820160cf6f65e4
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-07 16:59:40 +0200

    CFunctionCallPrinter should print arguments

--- lib/crask/c_function_call_printer.rb
+++ lib/crask/c_function_call_printer.rb
@@ -1,7 +1,10 @@
 module CRask
   class CFunctionCallPrinter
+    def initialize expr_printer
+      @expr_printer = expr_printer
+    end
     def print func
-      func.name + "()"
+      "#{func.name}(#{func.args.map { |a| @expr_printer.print a }.join(", ")})"
     end
   end
 end
--- spec/crask/c_function_call_printer_spec.rb
+++ spec/crask/c_function_call_printer_spec.rb
@@ -2,10 +2,19 @@ require 'crask/c_function_call_printer'
 module CRask
   describe :CFunctionCallPrinter do
     before(:each) do
-      @printer = CFunctionCallPrinter.new
+      @expr_printer = double("expression printer")
+      @printer = CFunctionCallPrinter.new @expr_printer
     end
     it "should print function name and parens" do
       @printer.print(CAst::FunctionCall.new("func", [])).should eql("func()")
+    end
+    it "should print arguments" do
+      @expr_printer.should_receive(:print).with(:first).and_return("FIRST")
+      @expr_printer.should_receive(:print).with(:second).and_return("SECOND")
+      @expr_printer.should_receive(:print).with(:third).and_return("THIRD")
+      func = CAst::FunctionCall.new("func", [ :first, :second, :third ])
+
+      @printer.print(func).should end_with("(FIRST, SECOND, THIRD)")
     end
   end
 end
