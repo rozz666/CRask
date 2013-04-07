9510c433302531036c9a74848dcedce299549641
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-04-07 18:09:49 +0200

    CFunctionArgumentPrinter should print type-name pairs separated by commas

--- lib/crask/c_function_argument_printer.rb
+++ lib/crask/c_function_argument_printer.rb
@@ -1,7 +1,7 @@
 module CRask
   class CFunctionArgumentPrinter
     def print args
-      ""
+      args.map{ |a| "#{a.type} #{a.name}" }.join(", ")
     end
   end
 end
--- spec/crask/c_function_argument_printer_spec.rb
+++ spec/crask/c_function_argument_printer_spec.rb
@@ -7,5 +7,11 @@ module CRask
     it "should print nothing for empty list" do
       @printer.print([]).should eql("")
     end
+    it "should print type-name pairs separated by commas" do
+      args = [
+        CAst::LocalVariable.new("type1", "name1"),
+        CAst::LocalVariable.new("type2", "name2") ]
+      @printer.print(args).should eql("type1 name1, type2 name2")
+    end
   end
 end
