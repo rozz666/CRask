dbc40005ac065c09c1477009cb83dd77adba5158
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-05-12 18:53:09 +0200

    CodeGenerator.generate_ast should generate C module AST

--- lib/crask/code_generator.rb
+++ lib/crask/code_generator.rb
@@ -33,5 +33,13 @@ module CRask
         @class_gen.generate_method_definitions s
       end.join
     end
+    
+    def generate_ast ast
+      global_vars = ast.stmts.map { |s| @class_gen.generate_declaration_ast(s) }
+      functions = ast.stmts.map { |s| @class_gen.generate_method_definitions_ast(s) }.flatten
+      registrations = ast.stmts.map { |s| @class_gen.generate_registration_ast(s) }.flatten
+      functions << CAst::Function.new("int", "main", [], [], registrations)
+      CAst::Module.new([ "crask.h", "stdarg.h" ], global_vars, functions)
+    end
   end
 end
--- spec/crask/code_generator_spec.rb
+++ spec/crask/code_generator_spec.rb
@@ -58,5 +58,30 @@ module CRask
         @cg.generate_method_definitions(ast).should eql("def1;def2;")
       end
     end
+    context "generate_ast" do
+      it "should generate C module AST" do
+        ast = Ast::Ast.with_two_classes("A", "B")
+        @class_gen.should_receive(:generate_declaration_ast).with(ast.stmts[0]).and_return(:decl1)
+        @class_gen.should_receive(:generate_declaration_ast).with(ast.stmts[1]).and_return(:decl2)
+        @class_gen.should_receive(:generate_method_definitions_ast).with(ast.stmts[0]).and_return([ :func1, :func2 ])
+        @class_gen.should_receive(:generate_method_definitions_ast).with(ast.stmts[1]).and_return([ :func3, :func4 ])
+        @class_gen.should_receive(:generate_registration_ast).with(ast.stmts[0]).and_return([ :reg1, :reg2 ])
+        @class_gen.should_receive(:generate_registration_ast).with(ast.stmts[1]).and_return([ :reg3, :reg4 ])
+          
+        c_module = @cg.generate_ast ast
+        c_module.should be_a_kind_of(CAst::Module)
+        c_module.includes.should eql([ "crask.h", "stdarg.h" ])
+        c_module.global_variables.should eql([ :decl1, :decl2 ])
+        c_module.should have(5).functions
+        c_module.functions[0..3].should eql([ :func1, :func2, :func3, :func4 ])
+        c_main = c_module.functions[4]
+        c_main.should be_a_kind_of(CAst::Function)
+        c_main.type.should eql("int")
+        c_main.name.should eql("main")
+        c_main.should have(0).arguments
+        c_main.should have(0).local_variables
+        c_main.statements.should eql([ :reg1, :reg2, :reg3, :reg4 ])
+      end
+    end
   end
 end
