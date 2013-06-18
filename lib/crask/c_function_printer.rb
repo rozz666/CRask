d18f349ecc176b1d642d36cd6fa44f29bdba1959
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-18 19:25:35 +0200

    CFunctionPrinter should print all functions

--- lib/crask/c_function_printer.rb
+++ lib/crask/c_function_printer.rb
@@ -5,13 +5,16 @@ module CRask
       @local_variable_printer = local_variable_printer
       @statement_printer = statement_printer
     end
-    def print function
+    def print functions
+      functions.map { |f| print_function f }.join
+    end
+    private
+    def print_function function
       args = @argument_printer.print function.arguments
       local_vars = ident_text(@local_variable_printer.print(function.local_variables))
       statements = ident_text(@statement_printer.print(function.statements))
       "#{function.type} #{function.name}(#{args}) {\n#{local_vars}#{statements}}\n"
     end
-    private
     def ident_text text
       text.split("\n").map { |line| "    " + line + "\n" }.join
     end    
--- spec/crask/c_function_printer_spec.rb
+++ spec/crask/c_function_printer_spec.rb
@@ -16,26 +16,35 @@ module CRask
       function = CAst::Function.with_type_name_and_args "type", "name", :args
       @arg_printer.should_receive(:print).with(:args).and_return("ARGS")
             
-      @printer.print(function).should eql("type name(ARGS) {\n}\n")
+      @printer.print([ function ]).should eql("type name(ARGS) {\n}\n")
     end
     it "should print local variables and statements" do
       function = CAst::Function.with_local_vars_and_stmts :local_vars, :statements
       @local_variable_printer.should_receive(:print).with(:local_vars).and_return("LOCAL_VARS\n")
       @statement_printer.should_receive(:print).with(:statements).and_return("STATEMENTS\n")
             
-      @printer.print(function).should include("{\n    LOCAL_VARS\n    STATEMENTS\n}")
+      @printer.print([ function ]).should include("{\n    LOCAL_VARS\n    STATEMENTS\n}")
     end
     it "should ident local variables" do
       function = CAst::Function.with_local_vars :local_vars
       @local_variable_printer.should_receive(:print).with(:local_vars).and_return("LINE1\nLINE2\n")
             
-      @printer.print(function).should include("{\n    LINE1\n    LINE2\n}")
+      @printer.print([ function ]).should include("{\n    LINE1\n    LINE2\n}")
     end
     it "should ident statements" do
       function = CAst::Function.with_stmts :statements
       @statement_printer.should_receive(:print).with(:statements).and_return("LINE1\nLINE2\n")
             
-      @printer.print(function).should include("{\n    LINE1\n    LINE2\n}")
+      @printer.print([ function ]).should include("{\n    LINE1\n    LINE2\n}")
+    end
+    it "should print all functions" do
+      functions = [
+        CAst::Function.with_type_name_and_args("type1", "name1", :args1),
+        CAst::Function.with_type_name_and_args("type2", "name2", :args2) ]
+      @arg_printer.should_receive(:print).with(:args1).and_return("ARGS1")
+      @arg_printer.should_receive(:print).with(:args2).and_return("ARGS2")
+            
+      @printer.print(functions).should eql("type1 name1(ARGS1) {\n}\ntype2 name2(ARGS2) {\n}\n")
     end
   end
 end
