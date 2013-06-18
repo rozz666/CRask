3f0a74bad3c79b4f0c04ba9d2a080432a0ce95f3
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-18 19:10:50 +0200

    CExpressionPrinter should print a function call

--- lib/crask/c_expression_printer.rb
+++ lib/crask/c_expression_printer.rb
@@ -5,25 +5,31 @@ require 'crask/cast/string'
 module CRask
   module CAst
     class Variable
-      def print
+      def print function_call_printer
         name
       end
     end
     class VariableAddress
-      def print
+      def print function_call_printer
         "&#{name}"
       end
     end
     class String
-      def print
+      def print function_call_printer
         "\"#{value}\""
+      end
+    end
+    class FunctionCall
+      def print function_call_printer
+        function_call_printer.print self
       end
     end
   end
   
   class CExpressionPrinter
+    attr_writer :function_call_printer
     def print expr
-      expr.print
+      expr.print @function_call_printer
     end
   end
 end
--- spec/crask/c_expression_printer_spec.rb
+++ spec/crask/c_expression_printer_spec.rb
@@ -2,7 +2,9 @@ require 'crask/c_expression_printer'
 module CRask
   describe :CExpressionPrinter do
     before(:each) do
+      @function_call_printer = double("function call printer")
       @printer = CExpressionPrinter.new
+      @printer.function_call_printer = @function_call_printer
     end
     it "should print variable name" do
       @printer.print(CAst::Variable.new("var")).should eql("var")
@@ -12,6 +14,11 @@ module CRask
     end
     it "should print a string in quotation marks" do
       @printer.print(CAst::String.new("a string")).should eql("\"a string\"")
+    end
+    it "should print a function call" do
+      fc = CAst::FunctionCall.new "some", []
+      @function_call_printer.should_receive(:print).with(fc).and_return(:printed)
+      @printer.print(fc).should be(:printed)
     end
   end
 end
