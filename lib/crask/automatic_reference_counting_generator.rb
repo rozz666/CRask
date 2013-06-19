d15db6d4323c4d24ecefc31eb9ba56ad7dcf8bfe
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-19 21:06:52 +0200

    AutomaticReferenceCountingGenerator should retain variables after assignments

--- lib/crask/automatic_reference_counting_generator.rb
+++ lib/crask/automatic_reference_counting_generator.rb
@@ -1,7 +1,10 @@
 module CRask
   class AutomaticReferenceCountingGenerator
     def update_ast ast
-      ast.stmts[0].defs[0].stmts << Ast::RetainDef.new(ast.stmts[0].defs[0].stmts[0].left)
+      method = ast.stmts[0].defs[0]
+      stmts = []
+      method.stmts.each { |a| stmts << a << Ast::RetainDef.new(a.left) }
+      method.stmts = stmts
     end
   end
 end
--- spec/crask/automatic_reference_counting_generator_spec.rb
+++ spec/crask/automatic_reference_counting_generator_spec.rb
@@ -14,5 +14,16 @@ module CRask
       stmts[1].should be_a_kind_of(Ast::RetainDef)
       stmts[1].name.should eql("var")
     end
+    it "should retain variables after assignments" do
+      assignments = [ Ast::AssignmentDef.new("a", nil), Ast::AssignmentDef.new("b", nil) ]
+      ast = Ast::Ast.new([ Ast::ClassDef.new(nil, [ Ast::MethodDef.new(nil, nil, assignments) ]) ])
+      @gen.update_ast ast
+      ast.stmts[0].defs[0].should have(4).stmts
+      stmts = ast.stmts[0].defs[0].stmts 
+      stmts[0].should be(assignments[0])
+      stmts[1].name.should eql("a")
+      stmts[2].should be(assignments[1])
+      stmts[3].name.should eql("b")
+    end
   end
 end
