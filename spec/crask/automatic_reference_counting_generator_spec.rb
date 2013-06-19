518babcee57db2dea0cd123bb315f9b8f5349eb9
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-19 22:33:41 +0200

    AutomaticReferenceCountingGenerator should update all methods in a class

--- spec/crask/automatic_reference_counting_generator_spec.rb
+++ spec/crask/automatic_reference_counting_generator_spec.rb
@@ -5,9 +5,10 @@ module CRask
       @method_updater = double(" method updater")
       @gen = AutomaticReferenceCountingGenerator.new @method_updater
     end
-    it "should update a method in a class" do
-      @method_updater.should_receive(:update_ast).with(:method)
-      ast = Ast::Ast.new([ Ast::ClassDef.new(nil, [ :method ]) ])
+    it "should update all methods in a class" do
+      @method_updater.should_receive(:update_ast).with(:method1)
+      @method_updater.should_receive(:update_ast).with(:method2)
+      ast = Ast::Ast.new([ Ast::ClassDef.new(nil, [ :method1, :method2 ]) ])
       @gen.update_ast ast
     end
   end
