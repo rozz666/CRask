267c72f1bd9e69b1556d9dab5e9f2b4b46642214
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-20 09:28:49 +0200

    LocalVariableDetector should return variables in order definition

--- lib/crask/local_variable_detector.rb
+++ lib/crask/local_variable_detector.rb
@@ -1,7 +1,7 @@
 module CRask
   class LocalVariableDetector
     def find_local_variables stmts
-      []
+      stmts.map { |s| s.left }
     end
   end
 end
--- spec/crask/local_variable_detector_spec.rb
+++ spec/crask/local_variable_detector_spec.rb
@@ -7,5 +7,12 @@ module CRask
     it "should return nothing for no statements" do
       @detector.find_local_variables([]).should eql([])
     end
+    it "should return variables in order definition" do
+      stmts = [
+        Ast::AssignmentDef.new("x", nil),
+        Ast::AssignmentDef.new("b", nil),
+        Ast::AssignmentDef.new("c", nil) ]
+      @detector.find_local_variables(stmts).should eql(["x", "b", "c"])
+    end
   end
 end
