4e4b9e1b2ee7858e82303ac33d72a08c0922a300
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-20 21:22:29 +0200

    refactored AutomaticReferenceCountingMethodUpdater

--- lib/crask/automatic_reference_counting_method_updater.rb
+++ lib/crask/automatic_reference_counting_method_updater.rb
@@ -4,19 +4,30 @@ require 'crask/ast/release_def'
 module CRask
   class AutomaticReferenceCountingMethodUpdater
     def update_ast method
-      stmts = []
-      vars = []
-      method.stmts.each do |a|
-        var = a.left
-        if vars.index(var)
-          stmts << Ast::ReleaseDef.new(var)
-        else
-          vars << var
-        end
-        stmts << a << Ast::RetainDef.new(var)
+      builder = ReferenceCountedAssignmentBuilder.new
+      method.stmts.each { |a| builder.add_assignment a }
+      builder.add_epilog
+      method.stmts = builder.stmts
+    end
+    private
+    class ReferenceCountedAssignmentBuilder
+      attr_reader :stmts
+      def initialize
+        @stmts = []
+        @vars = []
       end
-      vars.reverse.each { |v| stmts << Ast::ReleaseDef.new(v) }
-      method.stmts = stmts
+      def add_assignment a
+        var = a.left
+        if @vars.index(var)
+          @stmts << Ast::ReleaseDef.new(var)
+        else
+          @vars << var
+        end
+        @stmts << a << Ast::RetainDef.new(var)
+      end
+      def add_epilog
+        @vars.reverse.each { |v| @stmts << Ast::ReleaseDef.new(v) }
+      end
     end
   end
 end
