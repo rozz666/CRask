4bf398c0aafcf47617d6358510c2defd24f84f72
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-22 23:09:10 +0200

    DestructorRegistrationGenerator should generate C AST of destructor registration using libcrask

--- spec/crask/cgen/destructor_registration_generator_spec.rb
+++ spec/crask/cgen/destructor_registration_generator_spec.rb
@@ -1,5 +1,17 @@
 require 'crask/cgen/destructor_registration_generator'
 module CRask
   describe :DestructorRegistrationGenerator do
+    before(:each) do
+      @symbol_name_gen = double("symbol name generator")
+      @gen = DestructorRegistrationGenerator.new @symbol_name_gen
+    end
+    it "should generate C AST of destructor registration using libcrask" do
+      @symbol_name_gen.should_receive(:get_dtor_name).with("className").and_return("dtorName")
+
+      dtor_reg = @gen.generate_ast("className", "classVarName")
+      dtor_reg.should be_a_C_function_call("crask_addDestructorToClass").with(2).args
+      dtor_reg.args[0].should be_a_C_variable_address("dtorName")
+      dtor_reg.args[1].should be_a_C_variable("classVarName")
+    end
   end
 end
