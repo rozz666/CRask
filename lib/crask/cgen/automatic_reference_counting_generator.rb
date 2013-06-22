aedc9f39419a850e8721649482574cbb25a9dca7
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-22 23:58:39 +0200

    added TODO

--- lib/crask/cgen/automatic_reference_counting_generator.rb
+++ lib/crask/cgen/automatic_reference_counting_generator.rb
@@ -3,7 +3,7 @@ module CRask
     def initialize method_updater
       @method_updater = method_updater
     end 
-    def update_ast ast
+    def update_ast ast #TODO move dispatching here
       ast.stmts.each { |s| s.defs.each { |m| @method_updater.update_ast m } }
     end
   end
