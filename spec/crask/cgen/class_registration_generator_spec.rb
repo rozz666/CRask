8cf873e5f1dcb4f71026aa0225d82059e97ed451
Rafal Przywarski <rafal.przywarski@svensson.com.pl>
2013-06-22 22:44:45 +0200

    ClassRegistrationGenerator should generate class variable declaration C AST

--- spec/crask/cgen/class_registration_generator_spec.rb
+++ spec/crask/cgen/class_registration_generator_spec.rb
@@ -1,5 +1,20 @@
 require 'crask/cgen/class_registration_generator'
 module CRask
   describe :ClassRegistrationGenerator do
+    before(:each) do
+      @symbol_name_gen = double("symbol name generator")
+      @config = double("configuration")
+      @config.stub(:class_type).and_return(:CLASS_TYPE)
+
+      @gen = ClassDeclarationGenerator.new @symbol_name_gen, @config
+    end
+    it "should generate class variable declaration C AST" do
+      cdef = Ast::ClassDef.with_name "Z"
+      @symbol_name_gen.should_receive(:get_class_name).with("Z").and_return("className")
+      var = @gen.generate_declaration_ast(cdef)
+      var.should be_a_kind_of(CAst::GlobalVariable)
+      var.type.should eql(:CLASS_TYPE)
+      var.name.should eql("className")
+    end
   end
 end
